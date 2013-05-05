a717a36e4e423b84a0a94ff5c9cda0d5dbc6c643
je7sen <loisthen@gmail.com>
2013-05-05 16:57:31 +0800

    Right Click Enabled
    Right click enabled when detect palm flipped.

--- VirtualTouchScreen/VirtualTouchScreen.cpp
+++ VirtualTouchScreen/VirtualTouchScreen.cpp
@@ -37,17 +37,23 @@ CvCapture* capture=cvCreateCameraCapture(0);
 
 //Structure to hold blobs
 CvBlobs blobs;
+CvBlobs blobs2;
 
 //Windows
 cvNamedWindow("Live",CV_WINDOW_AUTOSIZE);
 cvNamedWindow("Threshold Filter",CV_WINDOW_AUTOSIZE);
+cvNamedWindow("Threshold Filter2",CV_WINDOW_AUTOSIZE);
 cvNamedWindow("Control",CV_WINDOW_AUTOSIZE);
+cvNamedWindow("Control_RCE",CV_WINDOW_AUTOSIZE);
 
 //Image Variables
 IplImage *frame=cvCreateImage(cvSize(w,h),8,3); //Original Image
 IplImage *hsvframe=cvCreateImage(cvSize(w,h),8,3);//Image in HSV color space
 IplImage *labelImg=cvCreateImage(cvSize(w,h),IPL_DEPTH_LABEL,1);//Image Variable for blobs
 IplImage *threshy=cvCreateImage(cvSize(w,h),8,1); //Threshold image of yellow color
+IplImage *labelImg2=cvCreateImage(cvSize(w,h),IPL_DEPTH_LABEL,1);//Image Variable for blobs2
+IplImage *threshy2=cvCreateImage(cvSize(w,h),8,1); //Threshold image of skin color
+IplImage *threshy3=cvCreateImage(cvSize(w,h),8,1); //Threshold image of combine
 //------------------
 IplImage *threcpy=cvCreateImage(cvSize(w,h),8,1); //Copy to pass to image velocity finder
 IplImage *mhi=cvCreateImage(cvSize(w,h),32,1);    //Motion History Image update
@@ -61,6 +67,10 @@ int screeny = GetSystemMetrics(SM_CYSCREEN);
 //Variables for trackbars
 int h1=23;int s1=229;int v1=8;
 int h2=39;int s2=255;int v2=255;
+
+//Variables for trackbars 2
+int h3=70;int s3=62;int v3=0;
+int h4=98;int s4=110;int v4=61;
 
 //Variables for time counting
 double beginTime=0;
@@ -81,6 +91,7 @@ bool doubleclick = false;
 bool ToEnableLongClick = false;
 bool longclick = false;
 
+
 //Creating the trackbars
 cvCreateTrackbar("Hue_1","Control",&h1,255,0);
 cvCreateTrackbar("Hue_2","Control",&h2,255,0);
@@ -88,6 +99,14 @@ cvCreateTrackbar("Sat_1","Control",&s1,255,0);
 cvCreateTrackbar("Sat_2","Control",&s2,255,0);
 cvCreateTrackbar("Val_1","Control",&v1,255,0);
 cvCreateTrackbar("Val_2","Control",&v2,255,0);
+
+//Creating the trackbars 2
+cvCreateTrackbar("Hue_1","Control_RCE",&h3,255,0);
+cvCreateTrackbar("Hue_2","Control_RCE",&h4,255,0);
+cvCreateTrackbar("Sat_1","Control_RCE",&s3,255,0);
+cvCreateTrackbar("Sat_2","Control_RCE",&s4,255,0);
+cvCreateTrackbar("Val_1","Control_RCE",&v3,255,0);
+cvCreateTrackbar("Val_2","Control_RCE",&v4,255,0);
 
 
 while(1)
@@ -114,15 +133,27 @@ while(1)
 	//Thresholding the frame for yellow
 	cvInRangeS(hsvframe,cvScalar(h1,s1,v1),cvScalar(h2,s2,v2),threshy);
 
+	//Thresholding the fram for skin colour
+	cvInRangeS(hsvframe,cvScalar(h3,s3,v3),cvScalar(h4,s4,v4),threshy2);
+
 	
 	//Filtering the frame
 	cvSmooth(threshy,threshy,CV_MEDIAN,7,7);
 
+	//Filtering the frame
+	cvSmooth(threshy2,threshy2,CV_MEDIAN,7,7);
+
 	//Finding the blobs
 	unsigned int result=cvLabel(threshy,labelImg,blobs);
 
+	//Finding the blobs 2
+	unsigned int result2=cvLabel(threshy2,labelImg2,blobs2);
+
 	//Rendering the blobs
 	cvRenderBlobs(labelImg,blobs,frame,frame);
+
+	//Rendering the blobs
+	cvRenderBlobs(labelImg2,blobs2,frame,frame);
 
 	//-----------------------
 	//Copying the yellow thresh image to pass to change picture
@@ -132,9 +163,10 @@ while(1)
 	//Filtering the blobs
 	cvFilterByArea(blobs,60,500);
 
+	//Filtering the blobs
+	cvFilterByArea(blobs2,60,500);
+
 	// Start capturing cursor (found cursor on screen)
-	bool cursorCaptured = false;
-	bool cursorDisappear = false;
 	int x = 0;
 	int y = 0;
 	
@@ -167,7 +199,7 @@ while(1)
 	//counting for enable double clicks.
 	StopTime2 = endTime2 - beginTime2; 
 	
-	//disable double click if time elaspe more than 0.6 second
+	//disable double click if time elaspe more than 1.5 second
 	if(StopTime2 > 1.5 && doubleclick )
 	{
 		beginTime2 = 0;
@@ -211,8 +243,8 @@ while(1)
 		
 	}
 
-	//disable long click if time more than 3 second
-	if(StopTime3 > 6 && longclick )
+	//disable long click if time more than 4 second
+	if(StopTime3 > 4 && longclick )
 	{
 		mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
 		cout<<" release long click"<<endl;
@@ -253,15 +285,17 @@ while(1)
 		cout<<"X-CursorPosition: "<<x<<" Y-CursorPosition: "<<y<<endl;
 
 		//Moving the mouse pointer
-		cursorCaptured = true;
 		SetCursorPos(x,y);
-
+		
 		//release long mouse click
 		mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
 		
-		//record time 
+		
+		//record time for first click
 		endTime = timestamp;
 		
+		//enable time for double click if first click is trigger
 		if(doubleclick)
 		{
 			endTime2 = timestamp;
@@ -270,19 +304,39 @@ while(1)
 		
 	}
 
-	
-	StopTime = beginTime - endTime;
+	for (CvBlobs::const_iterator ti=blobs2.begin(); ti!=blobs2.end(); ++ti)
+		{
+		double moment10_2 = ti->second->m10;
+		double moment01_2 = ti->second->m01;
+		double area2 = ti->second->area;
 
-	//clear cvBlob
-	//blobs.clear();
-	cursorDisappear = true;
+		//Variable for holding position
+		int x2;
+		int y2;
+
+		//Calculating the current position
+		x2 = moment10_2/area2;
+		y2 = moment01_2/area2;
+
+		//Mapping to the screen coordinates
+		int x3=(int)(x2*screenx/w);
+		int y3=(int)(y2*screeny/h);
+
+		//trigger right mouse click event
+		mouse_event(MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0);
+		mouse_event(MOUSEEVENTF_RIGHTUP, x, y, 0, 0);
+		cout<<" Right click"<<endl;
+		}
 
 	
-	
 
+		
 	//Showing the images
 	cvShowImage("Live",frame);
+	cvAnd(threshy,threshy2,threshy3);
 	cvShowImage("Threshold Filter",threshy);
+	cvShowImage("Threshold Filter2",threshy2);
+	
 
 	//Escape Sequence
 	char c=cvWaitKey(33);
